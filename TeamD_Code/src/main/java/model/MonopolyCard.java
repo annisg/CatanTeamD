3103bf097424054e8747d5bcc175a10e8715837d
pavaniravella <>
2021-04-06 21:50:17 -0400

    hi this is a dumb change

--- TeamD_Code/src/main/java/model/MonopolyCard.java
+++ TeamD_Code/src/main/java/model/MonopolyCard.java
@@ -5,6 +5,9 @@ import java.util.ResourceBundle;
 each opponent must give you 2 cards of that type (brick, grain, ore, lumber, or wool)
  */
 
+/*
+junk change
+ */
 public class MonopolyCard extends DevelopmentCard {
 
     private ResourceBundle messages;
