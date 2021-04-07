09abfde4bbc52eea1864fd9f1f4e9b1aa7d1ba06
Tom Ahmed <you@example.com>
2021-04-07 17:05:25 -0400

    Fixed Year Of Plenty tests

--- TeamD_Code/src/test/java/model/YearOfPlentyCard_Tests.java
+++ TeamD_Code/src/test/java/model/YearOfPlentyCard_Tests.java
@@ -11,28 +11,26 @@ import org.junit.Test;
 public class YearOfPlentyCard_Tests {
 
     ResourceBundle messages = ResourceBundle.getBundle("messages", new Locale("en"));
-    
+
     @Test
     public void testYearOfPlentyName() {
         DevelopmentCard testCard = new YearOfPlentyCard(messages);
         assertEquals("Year of Plenty", testCard.getName());
     }
-    
+
     @Test
-    public void testUseYearOfPlentyCard() {
+    public void testUseYearOfPlentyCardRemovesCard() {
         Player player = EasyMock.mock(Player.class);
-        EasyMock.replay(player);
+        DevelopmentCard testCard = new YearOfPlentyCard(messages);
         
-        try {
-            DevelopmentCard testCard = new YearOfPlentyCard(messages);
-            testCard.use(player);
-            fail("Expected RuntimeException");
-        } catch (RuntimeException e) {
-            assertEquals("Year of Plenty Cards are unimplemented.", e.getMessage());
-        }
+        player.removeDevelopmentCard(testCard);
+        EasyMock.replay(player);
+
+        testCard.use(player);
+        
         EasyMock.verify(player);
     }
-    
+
     @Test
     public void testCanBePlayed() {
         DevelopmentCard testCard = new YearOfPlentyCard(messages);
