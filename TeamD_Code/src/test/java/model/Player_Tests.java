570072d8eff8dd3bd779a0f46d5510c9a667b331
Eric Hamilton <eric@hami.io>
2021-04-09 01:20:54 -0400

    Add test cases for Player.stealRandomResourceFrom() and Player.stealAllOfResourceFrom().

--- TeamD_Code/src/test/java/model/Player_Tests.java
+++ TeamD_Code/src/test/java/model/Player_Tests.java
@@ -2,10 +2,7 @@ package model;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import org.easymock.EasyMock;
 import org.junit.Test;
@@ -14,9 +11,9 @@ import exception.*;
 
 public class Player_Tests {
 
-    Resource[] resources = { Resource.GRAIN, Resource.BRICK, Resource.ORE, Resource.LUMBER, Resource.WOOL };
+    Resource[] resources = {Resource.GRAIN, Resource.BRICK, Resource.ORE, Resource.LUMBER, Resource.WOOL};
     ResourceBundle messages = ResourceBundle.getBundle("messages", new Locale("en"));
-    
+
     @Test
     public void testPlayerColors() {
 
@@ -313,153 +310,255 @@ public class Player_Tests {
         EasyMock.verify(card);
 
     }
-    
+
     @Test
     public void testIncrementKnightCount() {
-        
+
         Player testPlayer = new Player(PlayerColor.RED);
-        
+
         assertEquals(0, testPlayer.getNumKnights());
-        
-        for(int i = 1;  i < 5; i++) {
+
+        for (int i = 1; i < 5; i++) {
             testPlayer.incrementKnightCount();
             assertEquals(i, testPlayer.getNumKnights());
         }
-        
+
     }
-    
+
     @Test
     public void testRoadCount() {
         Player testPlayer = new Player(PlayerColor.RED);
         assertEquals(15, testPlayer.getRoadCount());
-        for(int i =  1; i < 14; i++) {
+        for (int i = 1; i < 14; i++) {
             testPlayer.decrementRoadCount();
             assertEquals(15 - i, testPlayer.getRoadCount());
         }
     }
-    
+
     @Test
     public void testSettlementCount() {
         Player testPlayer = new Player(PlayerColor.RED);
-        
+
         assertEquals(5, testPlayer.getInitialSettlementCount());
         assertEquals(5, testPlayer.getSettlementCount());
-        for(int i =  1; i < 5; i++) {
+        for (int i = 1; i < 5; i++) {
             testPlayer.decrementSettlementCount();
             assertEquals(5 - i, testPlayer.getSettlementCount());
         }
-        for(int i =  2; i < 5; i++) {
+        for (int i = 2; i < 5; i++) {
             testPlayer.incrementSettlementCount();
             assertEquals(i, testPlayer.getSettlementCount());
         }
     }
-    
+
     @Test
     public void testCityCount() {
         Player testPlayer = new Player(PlayerColor.RED);
-        
+
         assertEquals(4, testPlayer.getInitialCityCount());
         assertEquals(4, testPlayer.getCityCount());
-        for(int i =  1; i < 3; i++) {
+        for (int i = 1; i < 3; i++) {
             testPlayer.decrementCityCount();
             assertEquals(4 - i, testPlayer.getCityCount());
         }
     }
-    
+
     @Test
     public void testFindDevelopmentCardNotFound() {
         Player testPlayer = EasyMock.partialMockBuilder(Player.class)
-                            .addMockedMethod("getDevelopmentCards")
-                            .mock();
+                .addMockedMethod("getDevelopmentCards")
+                .mock();
         ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>();
         EasyMock.expect(testPlayer.getDevelopmentCards()).andReturn(cards);
         EasyMock.replay(testPlayer);
-        
+
         try {
             testPlayer.getDevelopmentCard(KnightCard.class);
             fail();
-        }catch(ItemNotFoundException e) {};
+        } catch (ItemNotFoundException e) {
+        }
+        ;
         EasyMock.verify(testPlayer);
     }
-    
+
     @Test
     public void testFindDevelopmentCardFoundButNotPlayable() {
         Player testPlayer = EasyMock.partialMockBuilder(Player.class)
-                            .addMockedMethod("getDevelopmentCards")
-                            .mock();
+                .addMockedMethod("getDevelopmentCards")
+                .mock();
         MonopolyCard correctCard = new MonopolyCard(messages);
-        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(correctCard, new VictoryPointCard(messages)));
+        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(correctCard,
+                new VictoryPointCard(messages)));
         EasyMock.expect(testPlayer.getDevelopmentCards()).andReturn(cards);
         EasyMock.replay(testPlayer);
-        
+
         try {
             testPlayer.getDevelopmentCard(MonopolyCard.class);
             fail();
-        }catch(ItemNotFoundException e) {};
+        } catch (ItemNotFoundException e) {
+        }
+        ;
         EasyMock.verify(testPlayer);
     }
-    
+
     @Test
     public void testFindDevelopmentCardFoundFirst() {
         Player testPlayer = EasyMock.partialMockBuilder(Player.class)
-                            .addMockedMethod("getDevelopmentCards")
-                            .mock();
+                .addMockedMethod("getDevelopmentCards")
+                .mock();
         MonopolyCard correctCard = new MonopolyCard(messages);
         correctCard.makePlayable();
-        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(correctCard, new VictoryPointCard(messages)));
+        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(correctCard,
+                new VictoryPointCard(messages)));
         EasyMock.expect(testPlayer.getDevelopmentCards()).andReturn(cards);
         EasyMock.replay(testPlayer);
-        
+
         assertEquals(correctCard, testPlayer.getDevelopmentCard(MonopolyCard.class));
         EasyMock.verify(testPlayer);
     }
-    
+
     @Test
     public void testFindDevelopmentCardFoundLast() {
         Player testPlayer = EasyMock.partialMockBuilder(Player.class)
-                            .addMockedMethod("getDevelopmentCards")
-                            .mock();
+                .addMockedMethod("getDevelopmentCards")
+                .mock();
         MonopolyCard correctCard = new MonopolyCard(messages);
         correctCard.makePlayable();
-        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(new VictoryPointCard(messages), correctCard));
+        ArrayList<DevelopmentCard> cards =
+                new ArrayList<DevelopmentCard>(Arrays.asList(new VictoryPointCard(messages), correctCard));
         EasyMock.expect(testPlayer.getDevelopmentCards()).andReturn(cards);
         EasyMock.replay(testPlayer);
-        
+
         assertEquals(correctCard, testPlayer.getDevelopmentCard(MonopolyCard.class));
         EasyMock.verify(testPlayer);
     }
-    
+
     @Test
     public void testFindDevelopmentCardMultiple() {
         Player testPlayer = EasyMock.partialMockBuilder(Player.class)
-                            .addMockedMethod("getDevelopmentCards")
-                            .mock();
+                .addMockedMethod("getDevelopmentCards")
+                .mock();
         MonopolyCard correctCard = new MonopolyCard(messages);
         correctCard.makePlayable();
-        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(correctCard, new MonopolyCard(messages)));
+        ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(correctCard,
+                new MonopolyCard(messages)));
         EasyMock.expect(testPlayer.getDevelopmentCards()).andReturn(cards);
         EasyMock.replay(testPlayer);
-        
+
         assertEquals(correctCard, testPlayer.getDevelopmentCard(MonopolyCard.class));
         EasyMock.verify(testPlayer);
     }
-    
+
     @Test
     public void testLetAllCardsBePlayed() {
         Player testPlayer = EasyMock.partialMockBuilder(Player.class)
-                            .addMockedMethod("getDevelopmentCards")
-                            .mock();
+                .addMockedMethod("getDevelopmentCards")
+                .mock();
         MonopolyCard mockedCard1 = EasyMock.strictMock(MonopolyCard.class);
         KnightCard mockedCard2 = EasyMock.strictMock(KnightCard.class);
         ArrayList<DevelopmentCard> cards = new ArrayList<DevelopmentCard>(Arrays.asList(mockedCard1, mockedCard2));
-        
+
         EasyMock.expect(testPlayer.getDevelopmentCards()).andReturn(cards);
         mockedCard1.makePlayable();
         mockedCard2.makePlayable();
         EasyMock.replay(testPlayer, mockedCard1, mockedCard2);
-        
+
         testPlayer.letAllDevelopmentCardsBePlayed();
         EasyMock.verify(testPlayer, mockedCard1, mockedCard2);
     }
 
+    @Test
+    public void testStealingFromNoResources() {
+        Random randomMock = EasyMock.strictMock(Random.class);
+        Player robber = new Player(PlayerColor.RED);
+        Player victim = new Player(PlayerColor.BLUE);
+        robber.setRandom(randomMock);
+        EasyMock.replay(randomMock);
+
+        Resource actual = robber.stealRandomResourceFrom(victim);
+
+        assertEquals(Resource.DESERT, actual);  // Desert used as null/none object
+        EasyMock.verify(randomMock);
+    }
+
+    @Test
+    public void testStealingFromWithResources() {
+        Random randomMock = EasyMock.strictMock(Random.class);
+        Player robber = new Player(PlayerColor.RED);
+        Player victim = new Player(PlayerColor.BLUE);
+
+        // Because resources are stored as a hashmap, removal is not a "conveniently" predicable order.
+        //  As such, only one type is used.
+        victim.giveResource(Resource.BRICK, 4);
+
+        robber.setRandom(randomMock);
+        EasyMock.expect(randomMock.nextInt(4)).andReturn(2);
+        EasyMock.replay(randomMock);
+
+        Resource actual = robber.stealRandomResourceFrom(victim);
+
+        assertEquals(Resource.BRICK, actual);
+        assertEquals(1, robber.getResourceCount(Resource.BRICK));
+        assertEquals(3, victim.getResourceHandSize());
+        EasyMock.verify(randomMock);
+    }
+
+    @Test
+    public void testStealingFromSelf() {
+        Random randomMock = EasyMock.strictMock(Random.class);
+        Player robber = new Player(PlayerColor.RED);
+
+        robber.giveResource(Resource.BRICK, 4);
+
+        robber.setRandom(randomMock);
+        EasyMock.expect(randomMock.nextInt(4)).andReturn(2);
+        EasyMock.replay(randomMock);
+
+        Resource actual = robber.stealRandomResourceFrom(robber);
+
+        assertEquals(Resource.BRICK, actual);
+        assertEquals(4, robber.getResourceCount(Resource.BRICK));
+        assertEquals(4, robber.getResourceHandSize());
+        EasyMock.verify(randomMock);
+    }
+
+    @Test
+    public void testStealingAll() {
+        Player robber = new Player(PlayerColor.RED);
+        Player victim = new Player(PlayerColor.BLUE);
+
+        robber.giveResource(Resource.BRICK, 1);
+
+        victim.giveResource(Resource.GRAIN, 3);
+        victim.giveResource(Resource.BRICK, 4);
+        victim.giveResource(Resource.ORE, 5);
+
+
+        int actual = robber.stealAllOfResourceFrom(victim, Resource.BRICK);
+
+        assertEquals(4, actual);
+        assertEquals(5, robber.getResourceCount(Resource.BRICK));
+        assertEquals(5, robber.getResourceHandSize());
+
+        assertEquals(0, victim.getResourceCount(Resource.BRICK));
+        assertEquals(3 + 5, victim.getResourceHandSize());
+    }
+
+    @Test
+    public void testStealingAllFromSelf() {
+        Player robber = new Player(PlayerColor.RED);
+
+        robber.giveResource(Resource.GRAIN, 3);
+        robber.giveResource(Resource.BRICK, 4);
+        robber.giveResource(Resource.ORE, 5);
+
+        int actual = robber.stealAllOfResourceFrom(robber, Resource.BRICK);
+
+        assertEquals(4, actual);
+        assertEquals(3, robber.getResourceCount(Resource.GRAIN));
+        assertEquals(4, robber.getResourceCount(Resource.BRICK));
+        assertEquals(5, robber.getResourceCount(Resource.ORE));
+        assertEquals(3 + 4 + 5, robber.getResourceHandSize());
+
+    }
 }
