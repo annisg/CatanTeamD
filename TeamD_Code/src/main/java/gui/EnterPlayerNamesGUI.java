7765b8b0e158a311bf7c40bedbcca11f6a7c12ea
Griffin Annis <annisg@rose-hulman.edu>
2021-04-21 18:01:48 -0400

    fixed build issue, removed unused class

--- TeamD_Code/src/main/java/gui/EnterPlayerNamesGUI.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-public class EnterPlayerNamesGUI {
-    int numberOfPlayers;
-    public EnterPlayerNamesGUI(int numberOfPlayers){
-        this.numberOfPlayers = numberOfPlayers;
-    }
-
-}
